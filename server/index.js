89481eaa8dc5f85cb98fc4d537edb11cb6530db1
Chin Kuay <kuaychin@gmail.com>
2020-04-03 11:11:36 -0500

    modified JSON

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const controller = require("./controller");
+const massive = require("massive");
+
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../public/build"));
+
+massive(process.env.CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+  })
+  .catch(err => console.log(err));
+
+app.get("/api/inventory", controller.read);
+app.post("/api/inventory", controller.create);
+app.delete("/api/inventory/:id", controller.delete);
+app.put("/api/inventory/:id", controller.update);
+app.get("/api/inventory/:id", controller.getOne);
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Big Brother listening localhost:${port}`);
+});
